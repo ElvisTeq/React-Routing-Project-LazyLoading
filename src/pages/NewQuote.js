47eab3738f32483f74efd076d10da00da2853c45
elvis <cenelvis@gmail.com>
2022-06-20 15:14:48 -0400

    Programmatic (Imperative) Nagivation, { useHistory }, (.push), (.replace)

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,8 +1,15 @@
+import { useHistory } from "react-router-dom"; // To redirect, navigate Route to another location
 import QuoteForm from "../components/quotes/QuoteForm";
 
 const NewQuote = () => {
+  const history = useHistory();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    history.push("/quotes"); // Redirecting Route
+    // .push => erases history and creates a new Route with changes (No go back button)
+    // .replace => replace history Route with changes (Can use go back button)
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
